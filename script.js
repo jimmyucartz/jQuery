cc37a861fca30fb0e155a9a6b387be5145aa314c
jimmyucartz <jimmypappachan@ucartz.com>
2024-03-18 16:43:10 +0530

    jQuery Selector

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-$(document).ready(() => {
-  $("p").mouseenter(function () {
+// $(document).ready(function () {
+$(() => {
+  $("section").mouseenter(function () {
     $(this).css("color", "red");
   });
-  $("p").mouseleave(function () {
+  $("section").mouseleave(function () {
     $(this).css("color", "initial");
+  });
+  $("#para1").click(function () {
+    $(this).hide();
+  });
+  $(".paras").click(function () {
+    $(this).css("backgroundColor", "wheat");
+    $(this).css("width", "100px");
+  });
+  $("button").click(() => {
+    $("p").hide();
   });
 });
